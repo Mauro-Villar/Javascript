96998820a2bff5fb784e21fb5fc520471c8d3312
Mauro Villar <mauro_guillermo@hotmail.com>
2022-08-09 22:27:01 -0300

    Pre entrega 2

--- js/main.js
+++ js/main.js
@@ -1,52 +1,120 @@
 //array de objetos para cada producto
-let productos = [
-    { id: 1, nombre: "Pizza", precio: 500, estado: true },
-    { id: 2, nombre: "Nuggets", precio: 50, estado: true },
-    { id: 3, nombre: "Empanadas", precio: 100, estado: true },
-    { id: 4, nombre: "Franui", precio: 400, estado: true },
+const productos = [
+    { id: 1, nombre: "Pizza", precio: 500, inCart: 0,estado: true },
+    { id: 2, nombre: "Empanadas", precio: 100, inCart: 0, estado: true },
+    { id: 3, nombre: "Nuggets", precio: 50, inCart: 0, estado: true },
+    { id: 4, nombre: "Franui", precio: 400, inCart: 0, estado: true }
   ];
   
-  console.log('Desafio Events');
   
-  //Login
-  //Declaro variables
-  const botonLogin = document.getElementById('sendLogin')
-  const user = document.getElementById('user')
-  const password = document.getElementById('password')
   
-  //Accion del boton de login para saludar y validar inputs de usuario
-  botonLogin.onclick = e => { 
-      if(validarInputs(user,password)===true){
-          saludar(user)
-      }else{
-          alert('Ingresar nombre de usuario y contraseña')
+  console.log("PreEntrega2");
+  
+  let carts = document.querySelectorAll('.add-cart');
+  
+  for(let i=0 ; i < carts.length; i++){
+      carts[i].addEventListener('click', () => {
+      cartNumbers(productos[i]);
+      totalCost(productos[i])
+    })
+  }
+  
+  function cartNumbers(product) {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    
+    productNumbers = parseInt(productNumbers);
+  
+    if(productNumbers){
+      localStorage.setItem('cartNumbers', productNumbers + 1)
+      document.querySelector('.cart span').textContent = productNumbers + 1;
+    }else{
+      localStorage.setItem('cartNumbers', 1)
+      document.querySelector('.cart span').textContent = 1;
+    }
+  
+    setItems(product);
+  }
+  
+  function setItems (product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+  
+    if(cartItems != null){
+      if(cartItems[product.nombre] == undefined){
+        cartItems = {
+          ...cartItems,
+          [product.nombre]: product
+        }
       }
-  }
-  
-  //Funcion para validar usuario y contraseña vacios
-  let validarInputs = (user,password) => {
-      if(user.value.length > 0 && password.value.length > 0){
-          return true
-      }else{
-          return false
+      cartItems[product.nombre].inCart += 1;
+    } else{
+      product.inCart = 1;
+      cartItems = {
+        [product.nombre]: product
       }
+    }
+  
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems))
+  
   }
   
-  //Funcion saluda al nombre del usuario ingresado
-  let saludar = (user) => {
-      alert(`Bienvenido ${user.value}`)
+  function totalCost(product){
+    let cartCost = localStorage.getItem('totalCost');
+  
+    if(cartCost != null){
+      cartCost = parseInt(cartCost);
+      localStorage.setItem('totalCost', cartCost + product.precio);
+    }else{
+      localStorage.setItem('totalCost', product.precio)
+    }
+  
   }
   
-  //Boton para mostrar el menu (array de productos)
-  const botonMenu = document.getElementById('verMenu')
-  const menu = document.getElementById('menu')
+  function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
   
-  botonMenu.onclick = event => {    
+    if(productNumbers){
+      document.querySelector('.cart span').textContent = productNumbers;
+    }
+  }
   
-      productos.forEach( (producto) => {    
-          const listarProductos = document.createElement('li')
-          listarProductos.innerText = producto.nombre
+  function mostrarCarrito(){
+    let cartItems = localStorage.getItem("productsInCart")
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products")
+    let totalCarrito = localStorage.getItem('totalCost');
   
-          menu.append(listarProductos)
+    if(cartItems && productContainer) {
+      productContainer.innerHTML= '' 
+      Object.values(cartItems).map(item => {
+        productContainer.innerHTML += `
+        <div class="product">
+          <ion-icon class="product-delete-icon" name="trash-outline"></ion-icon>
+          <span class="productSpan">${item.nombre}</span>
+        
+        <div class="price">$${item.precio},00</div>
+        <div class="quantity">
+          <ion-icon class="decreaseArrow" name="caret-back-circle-outline"></ion-icon>
+          <span>${item.inCart}</span>
+          <ion-icon class="increaseArrow" name="caret-forward-circle-outline"></ion-icon>
+        </div>
+        <div class="total"> $${item.inCart * item.precio},00 </div>
+        </div>
+        `
       })
+    }
+  
+    productContainer.innerHTML += `
+      <div class="carritoTotalContainer">
+        <h4 class="carritoTotalTitle">
+          Total Carrito
+        </h4>
+        <h4 class="totalCarrito">$${totalCarrito},00</h4>
+      </div>
+    `
   }
+  
+  //Corre la funcion siempre que inicia la pagina
+  mostrarCarrito();
+  //No pierdo la cant del carrito en el reload de la pagina
+  onLoadCartNumbers();
